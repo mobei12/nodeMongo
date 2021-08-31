6e23c78ab7d95d948ea17a6b87423093c7bf9dae
蒋军晖 <mobeigege@icloud.com>
2021-08-31 16:46:39 +0800

    1、优化记录，添加运动类型字段

--- src/exerciseList/exerciseRecordSchema.ts
+++ src/exerciseList/exerciseRecordSchema.ts
@@ -6,6 +6,7 @@ const userSchema = new mongoose.Schema({
 	number_of_times: Number, //运动分的次数
 	number_of_breaks: Number, //休息的次数
 	single_time: Number, //单次休息的时间s秒
+    exercise_type: String, //运动类型，目前只有jumpRope(跳绳)
 	ctime: Date
 });
 module.exports = userSchema;
