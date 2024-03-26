9dc76e68026c47e870cc64b50f19149512e4e5bb
mobei12 <505453856@qq.com>
2024-03-26 14:50:54 +0800

    1、修改用户注册信息缺失

--- src/user/index.ts
+++ src/user/index.ts
@@ -50,7 +50,7 @@ router.useAsync(methods.post, '/register', async (req: Request, res: Response) =
 		} else {
 			const saveDate: Date = new Date();
 			const _password = genPassword(String(password));
-			const saveResult = await createUser(Object.assign({username,_password}, { level:0,ctime: saveDate }));//默认等级为0。等级越高，权限越高
+			const saveResult = await createUser(Object.assign({username,password:_password}, { level:0,ctime: saveDate }));//默认等级为0。等级越高，权限越高
 			if(saveResult&&saveResult._id){
 				resObj.id = saveResult._id
 			}
